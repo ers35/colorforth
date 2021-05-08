b8071cc73e364c3131a39eeab3c491c05e89cb63
Philippe Brochard <hocwp@free.fr>
2021-05-08 22:10:35 +0200

    comment conf.tmpl.h

--- conf.tmpl.h
+++ conf.tmpl.h
@@ -1,11 +1,14 @@
 #ifndef __CONF_H
 #define __CONF_H
 
-
+// Embed the lib.cf inside colorforth binary
 #define __EMBED_LIB
+
+// Enable colors in the terminal
 #define __ECHO_COLOR
 
 
+// Various sizes
 #define STACK_SIZE 30
 #define R_STACK_SIZE 30
 
@@ -16,6 +19,7 @@
 
 #define TIB_SIZE 32
 
+// Extensions to load
 #define LOAD_EXTENTIONS                         \
   init_os_utils(state);                         \
   init_dict_utils(state);                       \
