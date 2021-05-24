08dbd7f5df2393935326b8adcfe54ea211991042
Philippe Brochard <hocwp@free.fr>
2021-05-24 18:37:35 +0200

    allow to parse words from command line

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 // The author disclaims copyright to this source code.
 #include <cf-stdio.h>
-
 #include "colorforth.h"
+
+void
+parse_command_line(struct state *state, int argc, char *argv[])
+{
+  for (int i = 1; i < argc; i++)
+  {
+    parse_from_string(state, argv[i], 0);
+    parse_from_string(state, " ", 0);
+  }
+}
 
 int
 main(int argc, char *argv[])
@@ -9,6 +18,8 @@ main(int argc, char *argv[])
   init_terminal();
 
   struct state *state = colorforth_newstate();
+
+  parse_command_line(state, argc, argv);
 
   while (!state->done)
   {
