3c488c0ffe5b354e264bbe6c0b304f757f9b755f
Philippe Brochard <philippe.brochard@nomadeducation.fr>
2021-10-26 23:16:56 +0200

    add random + fix naming

--- ext/math.c
+++ ext/math.c
@@ -5,66 +5,86 @@
 void
 div_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 / n1);
 }
 
 void
 mod_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 % n1);
 }
 
 void
 slash_mod_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 / n1); push(s->stack, n2 % n1);
 }
 
 void
 start_slash_fn(struct state *s)
 {
-  cell c = pop(s->stack);
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell c = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 * n1 / c);
 }
 
 void
 lshift_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 << n1);
 }
 
 void
 rshift_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 >> n1);
 }
 
 void
 or_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 || n1);
 }
 
 void
 and_fn(struct state *s)
 {
-  cell n1 = pop(s->stack);
-  cell n2 = pop(s->stack);
+  const cell n1 = pop(s->stack);
+  const cell n2 = pop(s->stack);
   push(s->stack, n2 && n1);
+}
+
+void
+rand_fn(struct state *s)
+{
+  push(s->stack, rand());
+}
+
+void
+srand_fn(struct state *s)
+{
+  const cell seed = pop(s->stack);
+  srand(seed);
+}
+
+void
+random_fn(struct state *s)
+{
+  const cell n = pop(s->stack);
+  push(s->stack, rand() % n);
 }
 
 void
@@ -78,4 +98,7 @@ init_math_utils(struct state *state)
   define_primitive_extension(state, "rshift", rshift_fn);
   define_primitive_extension(state, "or", or_fn);
   define_primitive_extension(state, "and", and_fn);
+  define_primitive_extension(state, "rand", rand_fn);
+  define_primitive_extension(state, "srand", srand_fn);
+  define_primitive_extension(state, "random", random_fn);
 }
